bf93e29ce7946e64e8cd080b98076d76f8d878b3
fb421421 <fb421421@gmail.com>
2014-12-15 03:21:00 +0800

    init2

--- TravelJournalIOS/CouchbaseService.m
+++ TravelJournalIOS/CouchbaseService.m
@@ -10,42 +10,41 @@
 
 @implementation CouchbaseService
 
-
-+ (instancetype)init
+- (instancetype)init
 {
-    return nil;
+    self = [super init];
+    if (self) {
+        _manager = [CBLManager sharedInstance];
+        if (!_manager) {
+            NSLog (@"Cannot create shared instance of CBLManager");
+        }
+        
+        NSLog (@"Manager created");
+        
+        NSError *error;
+        
+        // create a name for the database and make sure the name is legal
+        NSString *dbname = @"journals";
+        if (![CBLManager isValidDatabaseName: dbname]) {
+            NSLog (@"Bad database name");
+        }
+        
+        // create a new database
+        _database = [_manager databaseNamed: dbname error: &error];
+        if (!_database) {
+            NSLog (@"Cannot create database. Error message: %@",
+                   error.localizedDescription);
+        }
+        
+        // log the database location
+        NSString *databaseLocation =
+        [[[[NSBundle mainBundle] bundlePath] stringByDeletingLastPathComponent]
+         stringByAppendingString: @"/Library/Application Support/CouchbaseLite"];
+        NSLog(@"Database %@ created at %@", dbname,
+              [NSString stringWithFormat:@"%@/%@%@", databaseLocation, dbname, @".cblite"]);
+    }
+    return self;
 }
 
-- (void) initDB{
-    _manager = [CBLManager sharedInstance];
-    if (!_manager) {
-        NSLog (@"Cannot create shared instance of CBLManager");
-    }
-    
-    NSLog (@"Manager created");
-    
-    NSError *error;
-    
-    // create a name for the database and make sure the name is legal
-    NSString *dbname = @"journals";
-    if (![CBLManager isValidDatabaseName: dbname]) {
-        NSLog (@"Bad database name");
-    }
-    
-    // create a new database
-    _database = [_manager databaseNamed: dbname error: &error];
-    if (!_database) {
-        NSLog (@"Cannot create database. Error message: %@",
-               error.localizedDescription);
-    }
-    
-    // log the database location
-    NSString *databaseLocation =
-    [[[[NSBundle mainBundle] bundlePath] stringByDeletingLastPathComponent]
-     stringByAppendingString: @"/Library/Application Support/CouchbaseLite"];
-    NSLog(@"Database %@ created at %@", dbname,
-          [NSString stringWithFormat:@"%@/%@%@", databaseLocation, dbname, @".cblite"]);
-    
-}
 
 @end
